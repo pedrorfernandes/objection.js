df947184b4215cceb506c75339937c580e070b32
Sami Koskimäki <sami.koskimaki@vincit.fi>
2015-08-25 08:58:49 +0300

    generate objection.js API doc

--- tests/unit/relations/files/OwnerModel.js
+++
@@ -1,9 +0,0 @@
-var Model = require('../../../../lib/Model');
-
-function OwnerModel() {
-  Model.apply(this, arguments);
-}
-
-module.exports = Model.extend(OwnerModel);
-
-OwnerModel.tableName = 'OwnerModel';
